db56733e4ed2125944b89e01cf36a9e451dd36f5
Peter Dimov <pdimov@pdimov.com>
2015-05-27 01:29:50 +0300

    Move placeholders to namespace boost::placeholders.

--- include/boost/bind.hpp
+++ include/boost/bind.hpp
@@ -10,7 +10,7 @@
 //
 //  bind.hpp - binds function objects to arguments
 //
-//  Copyright (c) 2009 Peter Dimov
+//  Copyright (c) 2009, 2015 Peter Dimov
 //
 //  Distributed under the Boost Software License, Version 1.0.
 //  See accompanying file LICENSE_1_0.txt or copy at
@@ -20,5 +20,7 @@
 //
 
 #include <boost/bind/bind.hpp>
+
+using namespace boost::placeholders;
 
 #endif // #ifndef BOOST_BIND_HPP_INCLUDED

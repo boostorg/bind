9a59a15e28acb3560f262e767db68fc5f94ad6ba
Peter Dimov <pdimov@gmail.com>
2022-12-23 05:38:26 +0200

    Use BOOST_TEST_TRAIT_SAME

--- test/apply_test2.cpp
+++ test/apply_test2.cpp
@@ -4,12 +4,11 @@
 
 #include <boost/bind/apply.hpp>
 #include <boost/core/lightweight_test_trait.hpp>
-#include <boost/type_traits/is_same.hpp>
 
 int main()
 {
-    BOOST_TEST_TRAIT_TRUE((boost::is_same<void, boost::apply<void>::result_type>));
-    BOOST_TEST_TRAIT_TRUE((boost::is_same<int&, boost::apply<int&>::result_type>));
+    BOOST_TEST_TRAIT_SAME(void, boost::apply<void>::result_type);
+    BOOST_TEST_TRAIT_SAME(int&, boost::apply<int&>::result_type);
 
     return boost::report_errors();
 }

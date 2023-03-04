a421c908e3c1a1bf732e90e608ad72e95f18b195
Peter Dimov <pdimov@gmail.com>
2023-03-04 19:58:19 +0200

    Add notice for C++03 deprecation

--- include/boost/bind/bind.hpp
+++ include/boost/bind/bind.hpp
@@ -21,6 +21,7 @@
 //  See http://www.boost.org/libs/bind/bind.html for documentation.
 //
 
+#include <boost/bind/detail/requires_cxx11.hpp>
 #include <boost/config.hpp>
 #include <boost/bind/mem_fn.hpp>
 #include <boost/type.hpp>
@@ -28,7 +29,7 @@
 #include <boost/bind/arg.hpp>
 #include <boost/bind/detail/result_traits.hpp>
 #include <boost/bind/std_placeholders.hpp>
-#include <boost/detail/workaround.hpp>
+#include <boost/config/workaround.hpp>
 #include <boost/visit_each.hpp>
 #include <boost/core/ref.hpp>
 #include <boost/core/enable_if.hpp>
